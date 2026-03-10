90af7675b8777c7120d6883ab38091efbc5d96c0
Daiana <daianavieiradeandrade@gmail.com>
2026-03-09 23:39:33 -0300

    feat: initialize spring boot project

---
+++ src/main/java/com/royal/treasury/treasury/RoyalTreasuryApplication.java
@@ -0,0 +1,13 @@
+package com.royal.treasury.treasury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalTreasuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalTreasuryApplication.class, args);
+	}
+
+}
